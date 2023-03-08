45dd38d4a38950da981edd1e26e8f9ebc7e3a8fc
shaakir-23 <127210903+shaakir-23@users.noreply.github.com>
2023-03-08 22:21:43 +0300

    first commit

---
+++ loop.js
@@ -0,0 +1,6 @@
+for (i=0; i < 5; i++)
+{
+
+console.log(i);
+
+}
---
+++ myapp.js
@@ -0,0 +1,33 @@
+let x = 16;
+let marks = prompt("ENTER MARKS");
+alert(marks);
+
+if  (x > 10 || x < 20)
+
+{
+    if(x > 15)
+{
+
+
+    alert("true");
+    console.log("value when true");
+
+}
+else
+{
+
+
+}
+if ( > = 70)
+
+    alert("true");
+    console.log("value when FALSE");
+
+}
+else
+{
+
+alert("false");
+console.log("value when false");
+
+}
